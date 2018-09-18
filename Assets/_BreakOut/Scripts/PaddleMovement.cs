f4db2c209360de300a602d9622c31d21dc3e953d
nobu06 <nobu.rs06@gmail.com>
2018-09-18 12:31:06 +0900

    Add running game - prototype 01

---
+++ Assets/_BreakOut/Scripts/PaddleMovement.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * TODO
+ * - set limits on how far the paddle can go (-3 to 3 from the original position)
+ *   - perhaps use Math.Clamp  https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
+ *   - keep track of the initial position
+ */ 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+
+    public float speed = 1f;
+
+    private float xMvmt;
+
+    // set max and min position 3, -3
+
+
+	private void Start()
+	{
+        xMvmt = 0;
+	}
+
+	void Update () {
+        
+        xMvmt = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * xMvmt * speed * Time.deltaTime);
+	}
+}
